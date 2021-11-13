6977f9f3d90ce0b805f9bd2801b130d10ab205f6
sagar shingala <sagarss191@gmail.com>
2021-11-13 18:00:49 +0530

    code deploy

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ console.log('Initializing Server.',new Date().toString() );
 console.log("Environment: " +process.env.NODE_ENV);
 console.log("Loading Environment Constant: " +constant);
 
-
+ 
 app.use(cors());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
