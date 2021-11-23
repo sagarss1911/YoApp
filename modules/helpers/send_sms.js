33b8e0ad5bd1954a7341718b89917611331ac074
sagar shingala <sagarss191@gmail.com>
2021-11-23 20:25:02 +0530

    SMS gateway changes

--- modules/helpers/send_sms.js
+++ modules/helpers/send_sms.js
@@ -1,25 +1,8 @@
-let https = require('https'),
-    request = require('request');
-    url = require('url');
-
-
-
-let sms = (message, phone) => {    
-    var msgbody  ="Thanks for using Flocco! Your OTP is "+message+". Please do not share your OTP with anyone else. Have a great day."
-    var options = {
-      'method': 'POST',
-      'url': process.env.SMSURL,
-      'headers': {
-        'Authorization': process.env.SMSAUTHORIZATION,
-        'Content-Type': 'text/plain'
-      },
-      body: '{\r\n       "from": "Flocco",\r\n       "body": "'+msgbody+'",\r\n       "direct": false,	\r\n       "recipient": [    \r\n            {\r\n                "to": "'+phone+'"\r\n            }\r\n        ],\r\n       "type": "sms",\r\n       "reference": "XOXO",\r\n       "validity": "30",\r\n       "type_details": "",\r\n       "data_coding": "plain",\r\n       "flash_message": false,\r\n       "campaign_id": "43261606",\r\n       "template_id": 757\r\n} '
-    
-    };
-    request(options, function (error, response) {
-      if (error) throw new Error(error);
-      return response.body;
-    });
+let axios = require('axios');
+let sms = async (message, phone) => {   
+  var msgbody  ="Thanks for using YoApp! Your OTP is "+message+". Please do not share your OTP with anyone else. Have a great day."
+  let url = process.env.OTP_BASE_URL +"&username="+process.env.OTP_USERNAME+"&password="+process.env.OTP_PASSWORD+"&recipient="+phone+"&messagetype="+process.env.OTP_MESSAGETYPE+"&messagedata="+msgbody
+  return await axios.get(url);
 };
 
 module.exports = {
