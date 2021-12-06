e206bb92d9a36369809b2fc1bff697923f05c988
sagar shingala <sagarss191@gmail.com>
2021-12-06 18:15:49 +0530

    message app changes

--- modules/helpers/send_sms.js
+++ modules/helpers/send_sms.js
@@ -2,7 +2,7 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 let sms = async (message, phone) => {   
-  let msgbody  ="Thanks for using YoApp! Your OTP is "+message+". Please do not share your OTP with anyone else. Have a great day."
+  let msgbody  ="Thanks for using Alcophony, Your OTP is "+message+". Please do not share your OTP with anyone else. Have a great day."
   let sentmessage = await client.messages 
       .create({ 
          body: msgbody,  
