ed4f76b4bded243ed46d27654c2b2bbf4781c40d
sagar shingala <sagarss191@gmail.com>
2022-01-31 19:56:50 +0530

    pipeline

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,6 +11,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+pm2 stop app.js
 #install node modules
 npm install
 
