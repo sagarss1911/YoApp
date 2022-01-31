7f30520451d1fd3096d82dea5c503707335055b7
sagar shingala <sagarss191@gmail.com>
2022-01-31 20:28:34 +0530

    test7

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
-DIR="/home/ec2-user/YoApp"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
-cd /home/ec2-user/YoApp
-pm2 stop app.js
