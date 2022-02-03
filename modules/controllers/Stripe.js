dce54f246c57780c4fa645bc3ad3791076a42470
sagar shingala <sagarss191@gmail.com>
2022-02-03 11:08:33 +0530

    setup_intent API added

--- modules/controllers/Stripe.js
+++ modules/controllers/Stripe.js
@@ -66,7 +66,66 @@
          })
          .catch(next);
  }
-   
+   /**
+  * @swagger
+  * /api/v1/stripe/setup_intent:
+  *   get:
+  *     summary: Create Setup Intent.
+  *     tags:
+  *      - Stripe
+  *     parameters :
+  *     - name: x-auth-api-key
+  *       in: header   
+  *       description: an authorization header
+  *       required: true
+  *       type: string
+  *     - name: x-auth-token
+  *       in: header   
+  *       description: an authorization header
+  *       required: true
+  *       type: string 
+  *     - name: Accept-Language
+  *       in: header   
+  *       description: Language
+  *       required: false
+  *       type: string    
+  *     responses:
+  *       200:
+  *         description: user object
+  *         content:
+  *           application/json:
+  *             schema:
+  *               type: object
+  *               properties:
+  *                 data:
+  *                   type: object
+  *       400:
+  *         description: error in request processing
+  *         content:
+  *           application/json:
+  *             schema:
+  *               type: object
+  *               properties:
+  *                 message:
+  *                   type: string
+  *                 status:
+  *                   type: integer
+  *                   example: 400
+ */
+ let setupIntent = (req, res, next) => {
+    let userid = req.user ? req.user.userId : null;
+     return stripeManager
+         .setupIntent(userid)
+         .then(data => {
+             let result = {
+                 status: 200,
+                 data: data
+             }
+             return res.json(result);
+         })
+         .catch(next);
+ }
  module.exports = {    
-    ephemeralKeyCreate: ephemeralKeyCreate
+    ephemeralKeyCreate: ephemeralKeyCreate,
+    setupIntent:setupIntent
  };
