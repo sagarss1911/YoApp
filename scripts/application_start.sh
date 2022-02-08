6c09a2d8321cf626869134e93a19670923924859
sagar shingala <sagarss191@gmail.com>
2022-02-08 15:56:49 +0530

    scripting changed

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,7 +11,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-pm2 stop app.js
+pm2 kill
 #install node modules
 npm install
 
