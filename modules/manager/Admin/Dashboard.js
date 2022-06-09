77d896657af9cf534ade31bf8e73c54bde042bae
sagar shingala <sagarss191@gmail.com>
2022-06-09 16:49:44 +0530

    Cashtopup added in history

--- modules/manager/Admin/Dashboard.js
+++ modules/manager/Admin/Dashboard.js
@@ -3,24 +3,43 @@
 
 
 let BadRequestError = require('../../errors/badRequestError'),
-BankModal = require('../../models/Bank_transfer'),
-SupportModal = require('../../models/Admin/Support_request'),
+    BankModal = require('../../models/Bank_transfer'),
+    UserModal = require('../../models/Users'),
+    SupportModal = require('../../models/Admin/Support_request'),
 
     CustomQueryModel = require("../../models/Custom_query"),
     SequelizeObj = require("sequelize"),
     moment = require("moment");
 
 let getBankTransferReporting = async () => {
-    let todayStartDate = new Date(moment().startOf('date').format('YYYY-MM-DD hh:mm:ss'));    
+    let todayStartDate = new Date(moment().startOf('date').format('YYYY-MM-DD hh:mm:ss'));
     let startOfMonth = new Date(moment().startOf('month').format('YYYY-MM-DD hh:mm:ss'));
-    let endOfMonth   = new Date(moment().endOf('month').format('YYYY-MM-DD hh:mm:ss'));    
-
+    let endOfMonth = new Date(moment().endOf('month').format('YYYY-MM-DD hh:mm:ss'));
+    let totalUsers = await UserModal.count({
+        raw: true
+    })
+    let totalMerchants = await UserModal.count({
+        where: { isMerchant: 1 },
+        raw: true
+    })
+    let totalVerifiedMerchants = await UserModal.count({
+        where: { isMerchant: 1, isMerchantVerified: 1 },
+        raw: true
+    })
+    let totalEnabledMerchants = await UserModal.count({
+        where: { isMerchant: 1, isMerchantVerified: 1, isMerchantEnabled: 1 },
+        raw: true
+    })
+    let totalCashTopupEnabledMerchants = await UserModal.count({
+        where: { isMerchant: 1, isMerchantVerified: 1, isMerchantEnabled: 1, isCashTopupEnabled: 1 },
+        raw: true
+    })
     let totalBankTransferRequestToday = await BankModal.count({
-        where: { createdAt: { $gte: todayStartDate} },
+        where: { createdAt: { $gte: todayStartDate } },
         raw: true
     })
     let totalBankTransferPendingRequestToday = await BankModal.count({
-        where: {status:'pending', updatedAt: { $gte: todayStartDate} },
+        where: { status: 'pending', updatedAt: { $gte: todayStartDate } },
         raw: true
     })
     let totalBankTransferRequestForCurrentMonth = await BankModal.count({
@@ -28,40 +47,40 @@ let getBankTransferReporting = async () => {
         raw: true
     })
     let totalBankTransferPendingRequestForCurrentMonth = await BankModal.count({
-        where: { status:'pending',updatedAt: { $gte: startOfMonth, $lte: endOfMonth } },
+        where: { status: 'pending', updatedAt: { $gte: startOfMonth, $lte: endOfMonth } },
         raw: true
     })
-    
+
 
     let totalBankTransferRequesttillDate = await BankModal.count()
     let totalBankTransferPendingRequesttillDate = await BankModal.count({
-        where: { status:'pending' },
+        where: { status: 'pending' },
         raw: true
     })
 
     let totalBankTransferCompletedRequestToday = await BankModal.count({
-        where: {status:'completed', updatedAt: { $gte: todayStartDate} },
+        where: { status: 'completed', updatedAt: { $gte: todayStartDate } },
         raw: true
     })
     let totalBankTransferCompletedRequestForCurrentMonth = await BankModal.count({
-        where: {status:'completed', updatedAt: { $gte: startOfMonth, $lte: endOfMonth } },
+        where: { status: 'completed', updatedAt: { $gte: startOfMonth, $lte: endOfMonth } },
         raw: true
     })
     let totalBankTransferCompletedRequesttillDate = await BankModal.count({
-        where: {status:'completed' },
+        where: { status: 'completed' },
         raw: true
     })
 
     let totalSupportRequest = await SupportModal.count();
     let pendingSupportRequest = await SupportModal.count({
-        where: {status:'pending' },
+        where: { status: 'pending' },
         raw: true
     })
     let completedSupportRequest = await BankModal.count({
-        where: {status:'completed' },
+        where: { status: 'completed' },
         raw: true
     })
-    return { 
+    return {
         totalBankTransferRequestToday,
         totalBankTransferPendingRequestToday,
         totalBankTransferRequestForCurrentMonth,
@@ -73,7 +92,12 @@ let getBankTransferReporting = async () => {
         totalBankTransferCompletedRequesttillDate,
         totalSupportRequest,
         pendingSupportRequest,
-        completedSupportRequest
+        completedSupportRequest,
+        totalUsers,
+        totalMerchants,
+        totalVerifiedMerchants,
+        totalEnabledMerchants,
+        totalCashTopupEnabledMerchants
     }
 }
 
--- modules/manager/Users.js
+++ modules/manager/Users.js
@@ -490,17 +490,20 @@ let getTermsCondition = async (body) => {
 let getProfile = async (userid, req) => {
     let userData = await UserModel.findOne({ where: { id: userid }, attributes: ['user_unique_id', 'name', 'profileimage', 'username', 'email', 'merchantbalance', 'phone', 'region', 'latitude', 'longitude', 'gender', 'isactive', 'notification_token', 'isSound', 'isVibration', 'isNotification', 'isTermsConditionAccepted', 'language', 'customer_id', 'balance', 'membershipId', 'isMerchant', 'isMerchantVerified', 'isMerchantEnabled', 'merchant_name', 'merchant_phone', 'merchant_address', 'licence_proof', 'address_proof', 'utility_proof', 'upgraded_image1', 'upgraded_image2', 'upgraded_image3', 'upgraded_image4', 'membershipId', 'isUpgradeRequestSubmitted', 'isMerchantUpgraded', 'isCashTopupEnabled', 'cash_topup_limit'], raw: true });
     if (userData.membershipId) {
+        let merchantBalanceInfo = await CommonHelper.merchantCashTopupLimit(userid)        
         userData.planDetails = await PlansModel.findOne({ where: { id: userData.membershipId }, raw: true })
-        userData.cash_topup_limit = userData.cash_topup_limit ? userData.cash_topup_limit : userData.planDetails.cash_topup_limit
+        userData.merchantBalanceInfo = merchantBalanceInfo
     }
     return userData;
 }
 let getProfileById = async (uuid) => {
 
-    let userData = await UserModel.findOne({ where: { user_unique_id: uuid }, attributes: ['user_unique_id', 'name', 'profileimage', 'username', 'email', 'phone', 'region', 'latitude', 'merchantbalance', 'longitude', 'gender', 'isactive', 'notification_token', 'isSound', 'isVibration', 'isNotification', 'isTermsConditionAccepted', 'language', 'customer_id', 'balance', 'isMerchant', 'isMerchantVerified', 'isMerchantEnabled', 'merchant_name', 'merchant_phone', 'merchant_address', 'licence_proof', 'address_proof', 'utility_proof', 'upgraded_image1', 'upgraded_image2', 'upgraded_image3', 'upgraded_image4', 'membershipId', 'isUpgradeRequestSubmitted', 'isMerchantUpgraded', 'isCashTopupEnabled', 'cash_topup_limit'], raw: true });
-    if (userData.membershipId) {
+    let userData = await UserModel.findOne({ where: { user_unique_id: uuid }, attributes: ['id','user_unique_id', 'name', 'profileimage', 'username', 'email', 'phone', 'region', 'latitude', 'merchantbalance', 'longitude', 'gender', 'isactive', 'notification_token', 'isSound', 'isVibration', 'isNotification', 'isTermsConditionAccepted', 'language', 'customer_id', 'balance', 'isMerchant', 'isMerchantVerified', 'isMerchantEnabled', 'merchant_name', 'merchant_phone', 'merchant_address', 'licence_proof', 'address_proof', 'utility_proof', 'upgraded_image1', 'upgraded_image2', 'upgraded_image3', 'upgraded_image4', 'membershipId', 'isUpgradeRequestSubmitted', 'isMerchantUpgraded', 'isCashTopupEnabled', 'cash_topup_limit'], raw: true });
+    if (userData.membershipId) {       
         userData.planDetails = await PlansModel.findOne({ where: { id: userData.membershipId }, raw: true })
-        userData.cash_topup_limit = userData.cash_topup_limit ? userData.cash_topup_limit : userData.planDetails.cash_topup_limit
+        let merchantBalanceInfo = await CommonHelper.merchantCashTopupLimit(userData.id)
+        userData.merchantBalanceInfo = merchantBalanceInfo
+        
     }
     return userData;
 }
